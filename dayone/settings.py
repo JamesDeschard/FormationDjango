f7f0b6a9fc86427e895f10213074cb0b7248cf9f
James Deschard <james.deschard@gmail.com>
2022-06-10 11:52:30 +0100

    debug set to true

--- dayone/settings.py
+++ dayone/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--6hu^*yp@q$wn&31cfy2u6y82122+_utjdvbzljo(3bk*-95um'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
